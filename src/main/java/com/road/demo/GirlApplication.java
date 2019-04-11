5d25447148b11c2e5528a2cb432d50f1ec5f8de5
Ma Lu Lu <mll@e-teng.com.cn>
2019-04-11 17:29:05 +0800

    update xml 配置文件；启动类

--- src/main/java/com/road/demo/GirlApplication.java
+++ src/main/java/com/road/demo/GirlApplication.java
@@ -3,10 +3,17 @@ package com.road.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication将三个有用的注解组合在了一起
+ * 
+ * @Configaration
+ * @ComponentScan
+ * @EnableAutoConfiguration
+ */
 @SpringBootApplication
 public class GirlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GirlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GirlApplication.class, args);
+    }
 }
