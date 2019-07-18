c14ac1194d12dee6b64a189286a48b889331fa4f
Ma Lu Lu <mll@e-teng.com.cn>
2019-07-18 23:15:32 +0800

    联系commit

---
+++ src/test/java/com/road/demo/ProviderServiceTest.java
@@ -0,0 +1,26 @@
+package com.road.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * @Describe
+ * @Author Road
+ * @Date 2019/5/7 16:53
+ **/
+public class ProviderServiceTest {
+
+    public static void main(String[] args) {
+        boolean is = true;
+        ClassPathXmlApplicationContext context =
+            new ClassPathXmlApplicationContext(new String[] {"applicationProvider.xml"});
+        context.start();
+        System.out.println("Provider service registry success");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
