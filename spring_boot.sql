478273d63fe1e1379cafa0becf16b226218f4ee2
Jack Road <462752802@qq.com>
2019-09-25 17:34:29 +0800

    girl：数据库表结构脚本

--- spring_boot.sql
+++ spring_boot.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50626
  File Encoding         : 65001
 
- Date: 19/09/2019 09:07:15
+ Date: 25/09/2019 17:30:59
 */
 
 SET NAMES utf8mb4;
@@ -29,6 +29,25 @@ CREATE TABLE `bd_device`  (
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`device_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for bd_syn_terminal_data
+-- ----------------------------
+DROP TABLE IF EXISTS `bd_syn_terminal_data`;
+CREATE TABLE `bd_syn_terminal_data`  (
+  `meet_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',
+  `meet_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '会议名称',
+  `meet_type` int(10) NOT NULL COMMENT '会议类型',
+  `meet_service` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '会议服务地址url',
+  `meet_account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '会议账户',
+  `meet_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '会议密码',
+  `meet_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '会议token',
+  `isCheck` int(10) NOT NULL COMMENT '会议账户是否通过校验',
+  `state` int(10) NOT NULL COMMENT '会议状态',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`meet_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -57,6 +76,20 @@ CREATE TABLE `re_user_device`  (
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`user_device_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for re_user_meet
+-- ----------------------------
+DROP TABLE IF EXISTS `re_user_meet`;
+CREATE TABLE `re_user_meet`  (
+  `re_user_meet_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键id',
+  `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'userid',
+  `meet_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'meetid',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
+  `create_time` datetime(0) NOT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`re_user_meet_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
