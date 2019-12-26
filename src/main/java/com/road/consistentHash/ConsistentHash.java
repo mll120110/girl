fbe0efa48b46fc30684cd1e908269a6141680bd7
Ma Lu Lu <mll@e-teng.com.cn>
2019-12-26 21:31:36 +0800

    consistent hash

---
+++ src/main/java/com/road/consistentHash/ConsistentHash.java
@@ -0,0 +1,12 @@
+package com.road.consistentHash;
+
+/**
+ * @Describe
+ * @Author Road
+ * @Date 2019/12/26 21:19
+ **/
+public class ConsistentHash {
+    public static void main(String[] args) {
+        System.out.println("192.168.1.1:1111" + "192.168.1.1:1111".hashCode());
+    }
+}
