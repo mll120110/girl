30db7c7390cba80b7592644112278736562cdef6
Jack Road <462752802@qq.com>
2020-01-07 17:10:55 +0800

    算法：快速排序

--- src/main/java/com/road/LeeCode/QuickSortA.java
+++ src/main/java/com/road/LeeCode/QuickSortA.java
@@ -14,20 +14,28 @@ public class QuickSortA {
         index = a[i];
         // 从表的两端交替向中间扫描
         while (i < j) {
-            while (i < j && a[j] >= index)
+            while (i < j && a[j] >= index) {
                 j--;
-            if (i < j)
+            }
+            if (i < j) {
                 // 用比基准小的记录替换低位记录
                 a[i++] = a[j];
-            while (i < j && a[i] < index)
+            }
+            while (i < j && a[i] < index) {
                 i++;
-            if (i < j)
+            }
+
+            if (i < j) {
                 // 用比基准大的记录替换高位记录
                 a[j--] = a[i];
+            }
         }
-        a[i] = index;// 将基准数值替换回 a[i]
-        sort(a, low, i - 1); // 对低子表进行递归排序
-        sort(a, i + 1, hight); // 对高子表进行递归排序
+        // 将基准数值替换回 a[i]
+        a[i] = index;
+        // 对低子表进行递归排序
+        sort(a, low, i - 1);
+        // 对高子表进行递归排序
+        sort(a, i + 1, hight);
 
     }
 
