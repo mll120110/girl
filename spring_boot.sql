bafe83bf1318f72aed2f32bc3c411833fdc838ea
Jack Road <462752802@qq.com>
2020-04-11 11:01:22 +0800

    update--SQL

--- spring_boot.sql
+++ spring_boot.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50646
  File Encoding         : 65001
 
- Date: 10/04/2020 18:44:06
+ Date: 11/04/2020 10:59:59
 */
 
 SET NAMES utf8mb4;
@@ -29,7 +29,7 @@ CREATE TABLE `bd_device`  (
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`device_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '设备信息表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for bd_module
@@ -41,25 +41,25 @@ CREATE TABLE `bd_module`  (
   `modele_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '模块名字',
   `parent_module` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '父级模块code',
   `modele_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '模块路由',
-  `state` int(10) NOT NULL COMMENT '状态',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`module_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '功能模块表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for bd_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `bd_permission`;
 CREATE TABLE `bd_permission`  (
-  `permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `permission_code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `permission_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `state` int(10) NOT NULL,
-  `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键id',
+  `permission_code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限编码',
+  `permission_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限名字',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
+  `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`permission_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for bd_role
@@ -69,11 +69,11 @@ CREATE TABLE `bd_role`  (
   `role_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色id',
   `role_code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色编码',
   `role_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色name',
-  `state` int(10) NOT NULL COMMENT '状态',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for bd_syn_terminal_data
@@ -92,7 +92,7 @@ CREATE TABLE `bd_syn_terminal_data`  (
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`meet_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '会议基本信息表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for bd_user
@@ -102,27 +102,41 @@ CREATE TABLE `bd_user`  (
   `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户主键',
   `user_account` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '登录用户账号',
   `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户name',
-  `state` int(10) NOT NULL COMMENT '用户状态',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
   `permission` int(10) NOT NULL COMMENT '用户权限0超级管理员，1普通用户',
   `create_time` datetime(0) NOT NULL,
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`user_id`) USING BTREE,
   UNIQUE INDEX `index_user_id`(`user_id`, `state`, `permission`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for re_module_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `re_module_permission`;
 CREATE TABLE `re_module_permission`  (
-  `re_module_permission` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `module_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `state` int(10) NOT NULL,
-  `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  PRIMARY KEY (`re_module_permission`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  `re_module_permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'id',
+  `module_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '模块表主键id',
+  `permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限表主键id',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
+  `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`re_module_permission_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '模块与权限关系表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for re_role_rmp
+-- ----------------------------
+DROP TABLE IF EXISTS `re_role_rmp`;
+CREATE TABLE `re_role_rmp`  (
+  `re_role_permission_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'id',
+  `role_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色id',
+  `rmp_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '模块与权限表主键id',
+  `state` int(10) NOT NULL COMMENT '状态',
+  `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`re_role_permission_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色与re_module_permission关系表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for re_user_device
@@ -130,13 +144,13 @@ CREATE TABLE `re_module_permission`  (
 DROP TABLE IF EXISTS `re_user_device`;
 CREATE TABLE `re_user_device`  (
   `user_device_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户与设备关系id',
-  `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `device_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户表id',
+  `device_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '设备表id',
   `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`user_device_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户与设备关系表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for re_user_meet
@@ -150,7 +164,7 @@ CREATE TABLE `re_user_meet`  (
   `create_time` datetime(0) NOT NULL,
   `update_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`re_user_meet_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户与会议关系表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for re_user_role
@@ -160,10 +174,10 @@ CREATE TABLE `re_user_role`  (
   `bd_user_role_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户角色id',
   `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
   `role_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色id',
-  `state` int(10) NOT NULL COMMENT '状态',
+  `state` int(10) NOT NULL COMMENT '状态0无效，1有效',
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`bd_user_role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户与角色关系表' ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
