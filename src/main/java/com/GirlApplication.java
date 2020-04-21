6f4ca91f4c718d96d48b74756f721d915391cabf
Jack Road <462752802@qq.com>
2020-04-21 18:46:32 +0800

    mybatis-plus时注入mapper失败的问题

--- src/main/java/com/GirlApplication.java
+++ src/main/java/com/GirlApplication.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @EnableAutoConfiguration
  */
 @SpringBootApplication
+/**
+ * mybatis-plus时注入mapper失败的问题
+ */
+@MapperScan(value = "com.vision")
 public class GirlApplication {
 
     public static void main(String[] args) {
